05392b89da8e64a31a8c24d06a72cc2085792842
Neo <kyujin0704@gmail.com>
2022-03-18 03:03:33 -0400

    if lower,upper,spical,pw

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+1144414
 var lower =["a","b","c","d","e","f","g","h","i","j","k","l","n","m","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper =["A","B","C","D","E","F","G","H","I","J","K","L","N","M","O","P","Q","R","S","T","U","v","W","X","Y","Z"];
 var num =["0","1","2","3","4","5","6","7","8","9"];
@@ -28,17 +29,42 @@ writePassword();
 
 function generatePassword() {
   var pw = "";
+
   var userlength = window.prompt (" Choose 8-128 chars for your random PASSWORD ");
-  if(isNaN(userlength)) {
-    window.alert("MUST CHOOSE NUMBER!");
-    return generatePassword()
-  }
-  if(userlength < 8 || userlength > 128){
-    alert("Please choose 8-128 Chars!")
-    return generatePassword()
-  }
-
   
+  if ( userlength < 8 || userlength > 128) {
+
+    window.alert("Please choose 8-128 Chars!");
+
+    return generatePassword()
+  } 
+  else if  (isNaN(userlength)) {
+    
+  alert("Numbers only!");
+
+  }
+  if (lower) {
+    lower+=
+    window.confirm("Include lower case?");
+  }
+  if (upper) {
+    upper+=
+    window.confirm("Include Upper case?");
+  } 
+  if (num) {
+    num+=
+    window.confirm("Include Num?");
+  } 
+  if (special) {
+    special+=
+    window.confirm("Include Speical Chars?");
+  }
+
+  for ( var i =  0; i < userlength; i++){
+    pw += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return pw;
+  }
 
 
- }
+
