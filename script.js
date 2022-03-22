08e80689b41c03fd0b74fda57e97bf97cd9f3828
Neo <kyujin0704@gmail.com>
2022-03-21 23:21:11 -0400

    Final PW Gen 1.0

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lower =["a","b","c","d","e","f","g","h","i","j","k","l","n","m","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upper =["A","B","C","D","E","F","G","H","I","J","K","L","N","M","O","P","Q","R","S","T","U","v","W","X","Y","Z"];
-var num =["0","1","2","3","4","5","6","7","8","9"];
-var special =["!","#","$","%","^","&","*","(",")","-","_","=","+"];
-var chars = '';
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-
-  var password = generatePassword();
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-
-generateBtn.addEventListener("click", writePassword);
-
-writePassword();
+var generateBtn = document.querySelector("#generate")
+var lower = "abcdefghijklnmopqrstuvwxyz"
+var upper = "ABCDEFGHIJKLNMOPQRSTUVWXYZ"
+var num = "0123456789"
+var special = "!@#$%^&*()_+~\`|}{[]:;?><,./-="
+var chars = ""
 
 
 // Wrtie generatePassword funtion  to create random password betwwen 8-128 chars.
 function generatePassword() {
-  var pw = '';
+  var pw = ""
 
   var userLength = window.prompt (" Choose 8-128 chars for your random PASSWORD ");
   
@@ -64,7 +44,7 @@ function generatePassword() {
   } 
    if (userSpeical) {
     chars += special
-    
+
   }
 
   if (!userLower&&!userUpper&&!userNum&&!userSpeical) {
@@ -77,9 +57,23 @@ function generatePassword() {
   }
   
   return pw;
-
   }
 
 
 
+// Write password to the #password input
+function writePassword() {
 
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+writePassword();
